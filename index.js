730aa1f46aac62272cfbdca7a5ec6457abad0621
Mahendra <mahendrasr1999@gmail.com>
2022-07-18 19:08:01 +0530

    loggercreate

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 require("dotenv").config();
 
 const express = require("express");
+const winston = require("winston")
 
 const app = express();
 
 const mongoose = require("mongoose");
 
-const Booksroute = require("./routers/books")
+const Booksroute = require("./routers/books");
 
 //  ----------------  Middlewares  ----------------------------
 
 app.use(express.json());
-app.use(express.urlencoded ({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
+//  ----------------     Logger    ----------------------------
+
+const logger = winston.createLogger({
+  level: "info",
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.colorize({ all: true })),
+    }),
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: "exceptions.log" }),
+  ],
+});
 
 //  ----------------     Router    ----------------------------
 
-app.use('/api/books', Booksroute)
-
+app.use("/api/books", Booksroute);
 
 //  ----------------  Connection  ----------------------------
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }).then(() => {
-  console.log(`connected to mongoDb Atlas`);
-}).catch(error => {
-    console.log(`something went wrong`, error);
-})
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .then(() => {
+    logger.info(`connected to mongoDb Atlas`);
+  })
+  .catch((error) => {
+    logger.error(`something went wrong`, error);
+  });
 
 const PORT = process.env.PORT || 3300;
 
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+  logger.info(`server is running on port ${PORT}`);
 });
- 
